b9f1196d090a90cc0ce53661272cc65c54fcba9a
Dani <119754452+Daniehen@users.noreply.github.com>
2026-08-16 22:45:16 +0200

    Did tasks 06-11

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(Number.isInteger(a) == false ||  Number.isInteger(b) == false) {
+        return "ERROR";
+    }
+    if(a < 0 ||  b < 0) {
+        return "ERROR";
+    }
+    sum = 0;
+    if (b < a) {
+        let tmp = a;
+        a = b;
+        b = tmp;
+    }
+    for(let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
